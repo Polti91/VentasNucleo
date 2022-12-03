d066c9189962d0938e172255eab4dbb6f8dc856d
Pablo Chaves <chavespablo91@gmail.com>
2022-12-03 20:21:33 -0300

    Hice lo de js

--- js/javascript.js
+++ js/javascript.js
@@ -13,8 +13,8 @@ function enviarinfo(){
     var nombrecontacto=document.getElementById("namecontacto").value;
     var celcontacto=document.getElementById("celcontacto").value;
     var mailcontacto=document.getElementById("mailcontacto").value;
-    let name = razonsocial+"%0A"+cuit+"%0A"+iva+"%0A"+nombre+"%0A"+direccion+"%0A"+localidad+"%0A"+provincia+"%0A"+tellocal+"%0A"+cellocal+"%0A"+maillocal+"%0A"+ig+"%0A"+nombrecontacto+"%0A"+celcontacto+"%0A"+mailcontacto;
+    let name = "*Razón social:* "+razonsocial+"%0A"+"*Cuit:* "+cuit+"%0A"+"*Condición frente al IVA:* "+iva+"%0A"+"*Nombre de fantasia:* "+nombre+"%0A"+"*Dirección:* "+direccion+"%0A"+"*Localidad:* "+localidad+"%0A"+"*Provincia:* "+provincia+"%0A"+"*Tel. del local:* "+tellocal+"%0A"+"*Cel. del local:* "+cellocal+"%0A"+"*Mail del local:* "+maillocal+"%0A"+"*Instagram (IG) del local:* "+ig+"%0A"+"*Nombre de la persona de contacto:* "+nombrecontacto+"%0A"+"*Celular de la persona de contacto:* "+celcontacto+"%0A"+"*Mail del contacto:* "+mailcontacto;
     // Abrir nuevo tab
-    window.open(name, '_blank');
+    window.open('https://api.whatsapp.com/send?phone=5492235419261&text='+name, '_blank');
   
 }
