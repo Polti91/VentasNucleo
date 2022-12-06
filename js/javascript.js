3c88c974090069c2440abaea1ea2a6d37c819871
EnriqueArroyo <enrique.s.arroyo@gmail.com>
2022-12-06 04:11:14 -0300

    Alertas peolas

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,9 @@
+
+
+
+
 function enviarinfo(v){
+
     var today = new Date();
     var razonsocial=document.getElementById("rs").value;
     var cuit=document.getElementById("cuit").value;
@@ -39,28 +44,53 @@ function enviarinfo(v){
 
 
 
-
-
-
-        
     if (razonsocial.length=="" || cuit=="" || nombre=="" || direccion=="" || localidad=="" || provincia=="" || tellocal=="" || cellocal=="" || maillocal=="" || ig=="" || nombrecontacto=="" || celcontacto=="" || mailcontacto==""){
-      if (iva!="0"){
-        alert("Debe completar todos los campos.");
-      }
-      else{
-        alert("Debe seleccionar la condicion ante el IVA.");
-      }
-    } else if (!maillocal.includes("@") || !mailcontacto.includes("@") || !maillocal.includes(".") || !mailcontacto.includes(".")){
-      alert("El formato del mail es incorrecto. El formato debe ser similar a ejemplo@ejemplo.com");
-    } else if (esCUITValida(cuit) == false){
-      alert("CUIT invalido");   
-    }else if (tellocal.length<10 || cellocal.length<10 || celcontacto.length<10){
-      alert("Revise los valores asignados en las lenguetas.");
+       
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Debes completar todos los campos',
+        }) 
     }
-        else if (v==1) {
-        name = "Hola Henry! Te envio mis datos de registro:%0A"+today.toLocaleString()+"%0A%0A"+"*Razón social:* "+razonsocial+"%0A"+"*Cuit:* "+cuit4+"%0A"+"*Condición frente al IVA:* "+iva+"%0A"+"*Nombre de fantasia:* "+nombre+"%0A"+"*Dirección:* "+direccion+"%0A"+"*Localidad:* "+localidad+"%0A"+"*Provincia:* "+provincia+"%0A"+"*Tel. del local:* "+tellocal+"%0A"+"*Cel. del local:* "+cellocal+"%0A"+"*Mail del local:* "+maillocal+"%0A"+"*Instagram (IG) del local:* "+ig+"%0A"+"*Nombre de la persona de contacto:* "+nombrecontacto+"%0A"+"*Celular de la persona de contacto:* "+celcontacto+"%0A"+"*Mail del contacto:* "+mailcontacto;
+    else if(iva=="0"){
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Debe seleccionar la condicion ante el IVA.',
+      }) 
+    }
+    else if (!maillocal.includes("@") || !mailcontacto.includes("@") || !maillocal.includes(".") || !mailcontacto.includes(".")){
+      
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'El formato del mail es incorrecto. El formato debe ser similar a ejemplo@ejemplo.com',
+      }) 
+    } else if (esCUITValida(cuit) == false){
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'CUIT Invalido',
+      })  
+    }else if (tellocal.length<10 || cellocal.length<10 || celcontacto.length<10){
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Revise los valores asignados en las lenguetas.',
+      }) 
+    }
+    else{
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Completado con exito',
+        showConfirmButton: false,
+        timer: 1500
+      })
+      if (v==1) {
+        name = "Hola Henry! Te envio mis datos de registro:%0A"+today.toLocaleString()+"%0A%0A"+"*Razón social:* "+razonsocial+"%0A"+"*Cuit:* "+cuit4+"%0A"+"*Condición frente al IVA:* "+iva+"%0A"+"*Nombre de fantasia:* "+nombre+"%0A"+"*Dirección:* "+direccion+"%0A"+"*Localidad:* "+localidad+"%0A"+"*Provincia:* "+provincia+"%0A"+"*Tel. del local:* "+tellocal+"%0A"+"*Cel. del local:* "+cellocal+"%0A"+"*Mail del local:* "+maillocal+"%0A"+"*Instagram (IG) del local:* "+ig+"%0A"+"*Nombre de la persona de contacto:* "+nombrecontacto+"%0A"+"*Celular de la persona de contacto:* "+celcontacto+"%0A"+"*Mail del contacto:* "+mailcontacto;       
+        setTimeout(window.open, 2000, 'https://api.whatsapp.com/send?phone=5492235419261&text='+name, '_blank')
         
-        window.open('https://api.whatsapp.com/send?phone=5492235419261&text='+name, '_blank');
       } else if (v==2) {
         name = "Hola Rafael! Te envio mis datos de registro:%0A"+today.toLocaleString()+"%0A%0A"+"*Razón social:* "+razonsocial+"%0A"+"*Cuit:* "+cuit4+"%0A"+"*Condición frente al IVA:* "+iva+"%0A"+"*Nombre de fantasia:* "+nombre+"%0A"+"*Dirección:* "+direccion+"%0A"+"*Localidad:* "+localidad+"%0A"+"*Provincia:* "+provincia+"%0A"+"*Tel. del local:* "+tellocal+"%0A"+"*Cel. del local:* "+cellocal+"%0A"+"*Mail del local:* "+maillocal+"%0A"+"*Instagram (IG) del local:* "+ig+"%0A"+"*Nombre de la persona de contacto:* "+nombrecontacto+"%0A"+"*Celular de la persona de contacto:* "+celcontacto+"%0A"+"*Mail del contacto:* "+mailcontacto;
         
@@ -96,6 +126,9 @@ function enviarinfo(v){
         
         window.open('https://api.whatsapp.com/send?phone=5492235419261&text='+name, '_blank');
       } 
+    }
+    
+     
     
   
 }
